da8df296e37baecd9e4d8c930809a1244b6afe83
Mark Furman <m.furman@live.com>
2018-09-10 14:27:43 -0400

    First Commit

---
+++ mac_changer/mac_changer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import subprocess
+
+interface = input("interface > ")
+mac = input("mac > ")
+
+print("[+] Changing MAC address for " + interface + " to " + mac)
+
+subprocess.call("ifconfig " + interface + " down", shell=True)
+subprocess.call("ifconfig " + interface + " hw ether " + mac, shell=True)
+subprocess.call("ifconfig " + interface + " up", shell=True)
