c456c4330f2392af8cef69382d25bdd097b3a31b
m.furman@live.com <4D4m4nt1um$>
2018-09-16 21:41:30 -0400

    Updated code for network scanner

---
+++ network_scanner/network_scanner.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import scapy.all as scapy
+
+def scan(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    arp_request.show()
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    broadcast.show()
+    arp_request_broadcast = broadcast/arp_request
+    arp_request_broadcast.show()
+
+scan("192.168.1.1/24")
